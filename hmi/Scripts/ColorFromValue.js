39eb4bedfe4e525f04d503fca1f3a4f9590b4be7
Saajan Kuthu <kuthusaju@gmail.com>
2020-09-03 16:22:45 +1000

    Initial commit

---
+++ hmi/Scripts/ColorFromValue.js
@@ -0,0 +1,31 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var ColorFromValue = function (myValue) {
+        var myColorObject = {};
+
+        switch (myValue) {
+            case 1:
+                myColorObject.color = "Blue";
+                break;
+            case 2:
+                myColorObject.color = "RGB(140,70,255)";
+                break;
+            default:
+                myColorObject.color = "#11CCCC";
+        }
+
+        return myColorObject
+
+    };
+    
+    TcHmi.Functions.registerFunction('ColorFromValue', ColorFromValue);
+})(TcHmi);
